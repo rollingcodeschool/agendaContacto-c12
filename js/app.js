650129ac7dbc85c90300aa894afc0dfd0ff70d6f
Emilse Arias <emilsearias@gmail.com>
2025-05-19 11:19:30 -0300

    agregamos la funcion para crear un contacto nuevo

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// el usuario cliquea el boton agregar invocar a una funcion que muestre el modal
+function agregarContacto(){
+   const modalCrearContacto = new bootstrap.Modal(document.getElementById('crearContacto'));
+    //mostrar ventana modal
+    modalCrearContacto.show();
+
+}
+//el usuario completa el form y debo crear un objeto contacto
+
+//declaro variables
+const btnAgregarContacto = document.getElementById('btnAgregarContacto');
+const formularioCrearContacto = document.querySelector('form');
+
+//manejadores de eventos
+btnAgregarContacto.addEventListener('click', agregarContacto)
+formularioCrearContacto.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    console.log('aqui deberia crear un objeto contacto')
+})
