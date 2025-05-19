4a2dbbbe914895de2267e6a539f189f06c17d7ab
Emilse Arias <emilsearias@gmail.com>
2025-05-19 11:38:19 -0300

    agregamos la clase contacto y modificamos a app con el type module

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import Contacto from "./classContacto.js";
+
 // el usuario cliquea el boton agregar invocar a una funcion que muestre el modal
 function agregarContacto(){
    const modalCrearContacto = new bootstrap.Modal(document.getElementById('crearContacto'));
@@ -16,4 +18,7 @@ btnAgregarContacto.addEventListener('click', agregarContacto)
 formularioCrearContacto.addEventListener('submit', (e)=>{
     e.preventDefault();
     console.log('aqui deberia crear un objeto contacto')
+    //crear un objeto Contacto
+    const nuevoContacto = new Contacto('juan', 'perez','23423423','juanperez@mail.com')
+    console.log(nuevoContacto)
 })
