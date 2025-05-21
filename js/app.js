a9f5fed9fcbb1bf29efaaa6088bc5bcac3758a91
Emilse Arias <emilsearias@gmail.com>
2025-05-21 10:25:11 -0300

    creamos un contacto con la información del formulario

--- js/app.js
+++ js/app.js
@@ -1,24 +1,42 @@
 import Contacto from "./classContacto.js";
 
 // el usuario cliquea el boton agregar invocar a una funcion que muestre el modal
-function agregarContacto(){
-   const modalCrearContacto = new bootstrap.Modal(document.getElementById('crearContacto'));
-    //mostrar ventana modal
-    modalCrearContacto.show();
-
+function abrirModalContacto() {
+  const modalCrearContacto = new bootstrap.Modal(
+    document.getElementById("crearContacto")
+  );
+  //mostrar ventana modal
+  modalCrearContacto.show();
 }
-//el usuario completa el form y debo crear un objeto contacto
+
+function crearContacto() {
+//todo traer todos los datos del formulario validados
+//crear un objeto Contacto
+  const nuevoContacto = new Contacto(inputNombre.value, inputApellido.value, inputTelefono.value, inputEmail.value, inputImagen.value, inputNotas.value);
+  console.log(nuevoContacto);
+  //almacenar el objeto en la agenda
+  agenda.push(nuevoContacto)
+  console.log(agenda);
+}
+
 
 //declaro variables
-const btnAgregarContacto = document.getElementById('btnAgregarContacto');
-const formularioCrearContacto = document.querySelector('form');
+const btnAgregarContacto = document.getElementById("btnAgregarContacto");
+const formularioCrearContacto = document.querySelector("form");
+const agenda = [];
+const inputNombre = document.querySelector('#nombre');
+const inputApellido = document.querySelector('#apellido');
+const inputEmail = document.querySelector('#email');
+const inputTelefono = document.querySelector('#telefono');
+const inputNotas = document.querySelector('#notas');
+const inputImagen = document.querySelector('#imagen');
+
 
 //manejadores de eventos
-btnAgregarContacto.addEventListener('click', agregarContacto)
-formularioCrearContacto.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    console.log('aqui deberia crear un objeto contacto')
-    //crear un objeto Contacto
-    const nuevoContacto = new Contacto('juan', 'perez','23423423','juanperez@mail.com')
-    console.log(nuevoContacto)
-})
+btnAgregarContacto.addEventListener("click", abrirModalContacto);
+
+formularioCrearContacto.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //el usuario completa el form y debo crear un objeto contacto
+  crearContacto();
+});
